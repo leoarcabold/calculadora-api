ac0521f7adc9c360b502f1d74a4f0b5bbaff2643
Leandro Soares <leoarcabold@gmail.com>
2022-07-27 23:35:31 -0300

    incluido a classe variação percentual

---
+++ src/main/java/com/calculadorafinanceira/controller/ValorPercentualController.java
@@ -0,0 +1,21 @@
+package com.calculadorafinanceira.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.calculadorafinanceira.model.ValorFuturo;
+
+@RestController
+@RequestMapping(value = "/calculadora/valorfuturo")
+public class ValorPercentualController {
+
+	@PostMapping
+	public ValorFuturo CalculaValorFuturo(
+			@RequestParam(name = "capital") Float capital,
+			@RequestParam(name = "taxa") Float taxa, 
+			@RequestParam(name = "prazo") Float prazo) {
+		return new ValorFuturo(capital, taxa, prazo);
+	}
+}
---
+++ src/main/java/com/calculadorafinanceira/model/VariacaoPercentual.java
@@ -0,0 +1,35 @@
+package com.calculadorafinanceira.model;
+
+import org.springframework.stereotype.Component;
+import java.lang.Math;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@NoArgsConstructor
+public class VariacaoPercentual {
+	/*
+	 * Fómula para o cálculo de
+	 * variação percentual: V = (vf-vi)/100
+	 * V = variação
+	 * vf = valor final
+	 * vi = valor inicial
+	 * Por exemplo, um produto que custava R$10 
+	 * aumentou para R$15, 
+	 * Qual foi a variação percentual de aumento?
+	 */
+	
+	private Float variacao; 
+	private Float valorFinal; 
+	private Float valorInicial; 
+
+	public VariacaoPercentual (Float valorFinal,Float valorInicial) {
+		this.valorFinal = valorFinal;
+		this.valorInicial = valorInicial;
+		this.variacao = (valorFinal-valorInicial)*100;
+	}
+	
+}
