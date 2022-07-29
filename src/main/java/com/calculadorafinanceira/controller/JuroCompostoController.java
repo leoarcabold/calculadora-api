f034e987d8fbe12ae242da138fc71c24cfe2e535
Leandro Soares <leoarcabold@gmail.com>
2022-07-29 12:44:38 -0300

    atualização de método

--- src/main/java/com/calculadorafinanceira/controller/JuroCompostoController.java
+++ src/main/java/com/calculadorafinanceira/controller/JuroCompostoController.java
@@ -3,6 +3,7 @@ package com.calculadorafinanceira.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.calculadorafinanceira.model.JuroComposto;
@@ -13,6 +14,7 @@ public class JuroCompostoController {
 
 
 	@GetMapping
+	@ResponseBody
 	public JuroComposto JuroComposto(
 			@RequestParam(name = "capital") Float capital,
 			@RequestParam(name = "taxa") Float taxa,
